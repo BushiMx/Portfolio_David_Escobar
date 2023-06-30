4de06ada245c481b2c59b14ac75091e222ece146
David Escobar <davidescobarmx@gmail.com>
2023-06-30 12:03:40 -0600

    About
    Finalizacion del componente about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import './about.css';
+import Pic from '../../assets/pic_me01.webp';
+import {PiGraduationCapFill} from 'react-icons/pi'
+import {BsCodeSlash} from 'react-icons/bs'
+// import Pic_small from '../../assets/pic_me02.webp';
 
 const About = () => {
-  return (
-    <section id='about'>Acerca de mi</section>
+  return ( 
+    <section id='about'>
+      <h5>Hablemos un poco </h5>
+      <h1>Sobre mi</h1>
+    
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Pic} alt="David Escobar" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <PiGraduationCapFill className='about__icon'/>
+              <h5>Graduado</h5>
+              <small>ORACLE Next Education</small>
+            </article>
+
+            <article className='about__card'>
+              <BsCodeSlash className='about__icon'/>
+              <h5>FrontEnd</h5>
+              <small>Dev</small>
+            </article>
+
+            {/* <article className='about__card'>
+              <PiGraduationCapFill className='about__icon'/>
+              <h5>Graduado</h5>
+              <small>ORACLE Next Education</small>
+            </article> */}
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque dolor 
+            consequatur expedita sapiente nostrum, necessitatibus deleniti quidem aut nemo 
+            culpa doloremque eos, obcaecati, corporis soluta labore ipsum tenetur tempore?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Hablemos</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
