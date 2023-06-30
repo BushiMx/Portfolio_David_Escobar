0a725ae0d30505832059fcfebb2b3cc2473ad909
David Escobar <davidescobarmx@gmail.com>
2023-06-29 20:26:52 -0600

    Nav
    Componente Nav completo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,17 +6,19 @@ import {LiaDev} from 'react-icons/lia'
 import {BiCodeBlock} from 'react-icons/bi'
 import {SlPeople} from 'react-icons/sl'
 import {AiOutlineMessage} from 'react-icons/ai'
+import {useState} from "react"
 
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
         <nav>
-            <a href="#home"><AiOutlineHome/></a>
-            <a href="#about"><PiUserLight/></a>
-            <a href="#portfolio"><BiCodeBlock/></a>
-            <a href="#services"><LiaDev/></a>
-            <a href="#testimonials"><SlPeople/></a>
-            <a href="#contact"><AiOutlineMessage/></a>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><PiUserLight/></a>
+            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiCodeBlock/></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><LiaDev/></a>
+            <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><SlPeople/></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
         </nav>
     )
 }
