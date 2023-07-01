acf2e3aebefdfe4d76d3da53e108fc44b56976b9
David Escobar <davidescobarmx@gmail.com>
2023-07-01 00:29:37 -0600

    experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,37 +9,41 @@ const Experience = () => {
             <h2>Skills</h2>
 
             <div className="container experience__container">
-                
+
                 <div className="experience__frontend">
                     <h3>FrontEnd Development</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>HTML 5</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>CSS</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>JavaScript</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>React</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>NODE</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Bootstrap</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Atomic Design</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
+                            <h4>SCRUM Fundamentals</h4>
                         </article>
                     </div>
                 </div>
@@ -48,23 +52,23 @@ const Experience = () => {
                     <h3>BackEnd Development</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>MySQL</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Oracle Cloud Infrastructure</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Python</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Data Science en OCI</h4>
                         </article>
                         <article className="experience__details">
-                            <BsFillPatchCheckFill/>
+                            <BsFillPatchCheckFill className="experience__details-icon"/>
                             <h4>Oracle Analytics</h4>
                         </article>
                     </div>
