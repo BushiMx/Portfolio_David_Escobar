8efc9aad39e1cebaf3f45dc2ab9b92c46456b114
David Escobar <davidescobarmx@gmail.com>
2023-07-01 03:16:09 -0600

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,52 @@ import React from "react";
 import './portfolio.css'
 import img__proyecto01 from "../../assets/proyecto01.webp"
 
+const info = [
+    {
+        id:1,
+        image: img__proyecto01,
+        title: 'Proyecto 1',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+    {
+        id:2,
+        image: img__proyecto01,
+        title: 'Proyecto 2',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+    {
+        id:3,
+        image: img__proyecto01,
+        title: 'Proyecto 3',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+    {
+        id:4,
+        image: img__proyecto01,
+        title: 'Proyecto 4',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+    {
+        id:5,
+        image: img__proyecto01,
+        title: 'Proyecto 5',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+    {
+        id:6,
+        image: img__proyecto01,
+        title: 'Proyecto 6',
+        github: 'https://github.com/BushiMx',
+        demo: 'https://github.com/BushiMx'        
+    },
+
+]
+
 const Portfolio = () => {
     return (
         <section id="portfolio">
@@ -9,66 +55,22 @@ const Portfolio = () => {
             <h2>Portafolio</h2>
 
             <div className="container portfolio__container">
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={img__proyecto01} alt="Proyecto" />
-                    </div>
-                    <h3>Titulo del proyecto</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/BushiMx" className="btn" target="_blank">GitHub</a>
-                        <a href="https://github.com/BushiMx" className="btn btn-primary" target="_blank">Ver Demo</a>
-                    </div>
-                </article>
+                {
+                    info.map(({id, image, title, github, demo}) => {
+                        return(
+                            <article key={id} className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className="btn" target="_blank">GitHub</a>
+                                    <a href={demo} className="btn btn-primary" target="_blank">Ver Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
