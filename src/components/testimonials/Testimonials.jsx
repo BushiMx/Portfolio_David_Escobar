c0b72ecb3f646b85454ea412a8f64212f9338cea
David Escobar <davidescobarmx@gmail.com>
2023-07-03 16:03:04 -0600

    testimonials update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React from "react"; 
 import './testimonials.css'
 import avatar_mel from '../../assets/avatar_mel.jpg'
+import avatar_daniel from '../../assets/avatar_daniel.jpeg'
+import avatar_fred from '../../assets/avatar_fred.jpeg'
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -20,15 +22,15 @@ const clients_list = [
     },
     {
         id:2,
-        image: avatar_mel,
-        name: 'Mel Torres',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, doloremque doloribus incidunt quos atque dolorum neque veritatis est! Fuga ut animi aliquam velit iste nulla quod assumenda explicabo enim distinctio.'
+        image: avatar_daniel,
+        name: 'Daniel Martinez',
+        review: 'Es una persona que trabaja en equipo, con iniciativa, dedicado y con liderazgo.'
     },
     {
         id:3,
-        image: avatar_mel,
-        name: 'Mel Torres',
-        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, doloremque doloribus incidunt quos atque dolorum neque veritatis est! Fuga ut animi aliquam velit iste nulla quod assumenda explicabo enim distinctio.'
+        image: avatar_fred,
+        name: 'Fred Mastison',
+        review: 'Demian is a professional that is a pleasure to work with. A sincere detail oriented person who manages to balance a driving work ethic with a pleasant and enjoyable attitude. He is a pleasure to work with and I would recommend him to any business or other professional.'
     }
 ]
 
